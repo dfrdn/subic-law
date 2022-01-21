b7fe33f75752d67008d39f517e3249806d4cb683
FordanG <60175691+FordanG@users.noreply.github.com>
2022-01-22 00:19:14 +0800

    Auto Import Icon Components

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
   plugins: ['~/plugins/smooth-scroll.js', '~/plugins/scrollspy.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: ['~/components', '~/components/icons'],
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
