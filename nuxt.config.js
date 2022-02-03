fa4758dcbede79d5e21604619ee28e96d5b8db63
FordanG <60175691+FordanG@users.noreply.github.com>
2022-02-04 00:12:56 +0800

    Update Meta Tags and Site Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'subic-law',
+    title: 'Subic Law',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Subic Law is one of the leading law firms in Olongapo City, our attorneys can help you with all of your legal needs. The firm has been engaged in general law practice for more than 15 years.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
